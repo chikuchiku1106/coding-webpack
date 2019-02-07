de380f96158b7bfa1b16c368e2d1f216d50fa91f
大沼 峻徳 <ohnhistory@gmail.com>
2019-02-07 18:09:47 +0900

    cssのビルドを入れる

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -1,4 +1,5 @@
 import './pugImport';
+import '../css/base.scss';
 
 console.log('test');
 
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = merge(baseConfig, {
   mode: 'development',
@@ -23,6 +25,9 @@ const config = merge(baseConfig, {
   // },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
   ]
 });
 
@@ -33,5 +38,51 @@ for (const key in config.entry) {
     config.entry[key].unshift('webpack-hot-middleware/client');
   }
 }
+
+config.module.rules.push({
+  test: /\.(sass|scss)$/,
+  use: [
+    'css-hot-loader',
+    MiniCssExtractPlugin.loader,
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true,
+        // 0 => no loaders (default);
+        // 1 => postcss-loader;
+        // 2 => postcss-loader, sass-loader
+        importLoaders: 2
+      }
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+        plugins: [
+          autoprefixer({
+            browsers: [
+              'last 2 version',
+              'IE 11'
+            ]
+          })
+        ]
+      }
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true,
+        includePaths: [path.resolve('./node_modules/')]
+      }
+    },
+    // {
+    //   loader: 'sass-resources-loader',
+    //   options: {
+    //     sourceMap: true,
+    //     resources: [path.resolve('./src/css/resources/*.scss')]
+    //   }
+    // }
+  ]
+});
 
 module.exports = config;
