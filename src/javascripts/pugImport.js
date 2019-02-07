3953081be94232f3f5603cef85fbf4f0097e07a2
大沼 峻徳 <ohnhistory@gmail.com>
2019-02-07 18:14:19 +0900

    HMRのステータスログが邪魔なので削除する

--- src/javascripts/pugImport.js
+++ src/javascripts/pugImport.js
@@ -4,10 +4,6 @@ import '../pug/test.pug';
 if (module.hot) {
   module.hot.accept(console.error);
 
-  module.hot.addStatusHandler((status) => {
-    console.log('status', status);
-  });
-
   module.hot.accept(['../pug/index.pug'], () => {
     console.log('pug change');
   });
